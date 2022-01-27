3ac364a81b7f34a8df1c3d6b99aea5bb7af23532
HYJB22 <2681095614@qq.com>
2022-01-27 10:35:57 -0800

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+
+// javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testMarddownParse() throws IOException {
+        Path fileName = Path.of("another-file.md");
+	    String contents = Files.readString(fileName);
+
+        assertEquals(List.of("https://cse15L9amLab.com", "https://hahahah.html/subpage"), MarkdownParse.getLinks(contents) );
+
+    }
+
+}
