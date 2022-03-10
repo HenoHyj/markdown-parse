080e57a2610cc60042f78b75ad4bbc33171dd719
HYJB22 <2681095614@qq.com>
2022-03-10 09:39:23 -0800

    last lab

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,8 +1,12 @@
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
@@ -44,17 +48,23 @@ public class MarkdownParse {
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-        Map<String, List<String>> fileContents;
-        ArrayList<String> links;
-        if(new File(args[0]).isDirectory()){
-            fileContents = getLinks(new File(args[0]));
-            System.out.println(fileContents);
-        }
-		else{
+        // Map<String, List<String>> file;
+        // fileContents = file.
+        // ArrayList<String> links;
+        // if(new File(args[0]).isDirectory()){
+        //     fileContents = getLinks(new File(args[0]));
+        //     System.out.println(fileContents);
+        // }
+		// else{
+        //     Path fileName = Path.of(args[0]);
+	    //     String contents = Files.readString(fileName);
+        //     links = getLinks(contents);
+        //     System.out.println(links);
+        // }
+
             Path fileName = Path.of(args[0]);
 	        String contents = Files.readString(fileName);
             ArrayList<String> links = getLinks(contents);
             System.out.println(links);
-        }
     }
 }
